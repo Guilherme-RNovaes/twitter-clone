c7b41293eb3b4dff86f6ddba465d5401d4c0a4cd
guilh <guilherme00rogerio@gmail.com>
2023-05-01 21:08:30 -0300

    iniciando o projeto do twitter e primeiros componentes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+import "./global.css"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <div className="layout">
+      <aside className="sidebar"></aside>
+      <div className="content">
+        
+      </div>
+    </div>
   </React.StrictMode>,
 )
