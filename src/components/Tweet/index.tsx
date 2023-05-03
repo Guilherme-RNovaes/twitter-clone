abd29034792a0e55da0c5190116bb60274ab5206
guilh <guilherme00rogerio@gmail.com>
2023-05-03 18:15:12 -0300

    Estilização inicial dos Tweets

--- src/components/Tweet/index.tsx
+++ src/components/Tweet/index.tsx
@@ -1,9 +1,35 @@
+import { ArrowClockwise, ChatCircle, Heart } from 'phosphor-react'
 import './styles.css'
 
 function Tweet() {
   return (
-    <a href='#' className='Tweet'>
+    <a href='#' className='tweet'>
       <img src="https://github.com/Guilherme-RNovaes.png" alt="" />
+      <div className="tweet-content">
+        <div className="tweet-content-header">
+          <strong>Guilherme Novaes</strong>
+          <span>
+            @Guilherme-RNovaes
+          </span>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quibusdam cupiditate eos dolorem non inventore, mollitia exercitationem alias eveniet porro odio ea assumenda voluptatem iusto accusamus unde animi amet voluptas.
+        </p>
+        <div className="tweet-content-footer">
+          <button type='button'>
+            <ChatCircle />
+            20
+          </button>
+          <button type='button'>
+            <ArrowClockwise />
+            20
+          </button>
+          <button type='button'>
+            <Heart />
+            20
+          </button>
+        </div>
+      </div>
     </a>
   )
 }
